0ac7a62b33ba29c07d640934bd65deca00a27f39
sm hasanur <sm.hasanur121@gmail.com>
2022-03-26 00:31:59 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor tempora
+        explicabo totam corporis quidem consequuntur officiis facere quod natus.
+        Dolorum, saepe error. Pariatur nisi tenetur quisquam iusto distinctio,
+        odio reprehenderit?
+        <button className="btn">MasALlah</button>
+      </h2>
     </div>
   );
 }
